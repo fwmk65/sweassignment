0b87aa92243e7fc16833efa9e259bc08d79a2d01
RajjSinghh <gurrajsingh22@gmail.com>
2022-01-09 19:09:25 +0000

    Added some toy data to test search algorithm on

---
+++ project/api/toy.sql
@@ -0,0 +1,35 @@
+-- Just some toy data so that we can get pathfinding done without
+-- Having to have the real database yet
+-- The Graph looks like:
+
+-- 1 - 2 - 3
+--     | 
+--     4
+--     | 
+--     5
+
+
+CREATE TABLE rooms(
+    roomID INTEGER PRIMARY KEY,
+    adjacent TEXT
+);
+
+INSERT INTO rooms values(
+    1, "2"
+);
+
+INSERT INTO rooms values(
+    2, "1, 3, 4"
+);
+
+INSERT INTO rooms values(
+    3, "2"
+);
+
+INSERT INTO rooms values(
+    4, "2, 5"
+);
+
+INSERT INTO rooms values(
+    5, "4"
+);
