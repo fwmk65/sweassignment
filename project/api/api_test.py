d29ccc5c39ebbc823f64f8492de5334b448eb665
fwmk65 <fwmk65@durham.ac.uk>
2022-03-06 18:28:39 +0000

    refactored to fit pep8 compliance

--- project/api/api_test.py
+++ project/api/api_test.py
@@ -1,8 +1,11 @@
 import unittest
 from api import *
 
+
 class TestPathFinding(unittest.TestCase):
     def test_outputs(self):
-        self.assertEqual(type(Search(0, 4)), tuple)
-        distances, prev = Search(0, 4)
-        self.assertEqual(GeneratePath(0, 4, prev), [0, 1, 3, 4])
+        # Testing return type
+        self.assertEqual(type(search(0, 4)), tuple)
+        distances, prev = search(0, 4)
+        # Testing correctness of path
+        self.assertEqual(generate_path(0, 4, prev), [0, 1, 3, 4])
