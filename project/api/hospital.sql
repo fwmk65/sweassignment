39d714c7e6647e7cda796a8c79b644a1d523039b
fwmk65 <fwmk65@durham.ac.uk>
2022-03-14 03:05:51 +0000

    added hospital database#

---
+++ project/api/hospital.sql
@@ -0,0 +1,295 @@
+--Hospital SQL file
+--Each room will be added to a rooms table
+--Call this database hospital.db
+CREATE TABLE Rooms
+(
+        ID INTEGER,
+        Name TEXT,
+        Accessible Boolean,
+        Adjacent Text
+);
+
+INSERT INTO Rooms
+VALUES(
+                1, "a-block", 3, "5.52"
+);
+
+INSERT INTO Rooms
+VALUES(
+                2, "accident and emergency department", 1, "47,38"
+);
+
+INSERT INTO Rooms
+VALUES(
+                3, "admissions office", 1, "19,22"
+);
+
+INSERT INTO Rooms
+VALUES(
+                4, "ambulance liason office", 1, "14,46,43,38"
+);
+
+INSERT INTO Rooms
+VALUES(
+                5, "b-block", 1, "1,8"
+);
+
+INSERT INTO Rooms
+VALUES(
+                6, "board room", 1, "19,50"
+);
+
+INSERT INTO Rooms
+VALUES(
+                7, "boiler house", 1, "50"
+);
+
+INSERT INTO Rooms
+VALUES(
+                8, "c-block", 1, "5,12"
+);
+
+INSERT INTO Rooms
+VALUES(
+                9, "south east block", 1, "13,27"
+);
+
+INSERT INTO Rooms
+VALUES(
+                10, "d-block", 1, "48,49,37"
+);
+
+INSERT INTO Rooms
+VALUES(
+                11, "nutrition and diabetics", 1, "28,14"
+);
+
+INSERT INTO Rooms
+VALUES(
+                12, "carousel centre", 1, "8"
+);
+
+INSERT INTO Rooms
+VALUES(
+                13, "day surgery unit and operating thatres", 1, "9,19,30,23"
+);
+
+INSERT INTO Rooms
+VALUES(
+                14, "theatre admissions unit and children's surgical unit", 1, "4,11"
+);
+
+INSERT INTO Rooms
+VALUES(
+                15, "east ward block", 1, "30,53,29,3,43,19"
+);
+
+INSERT INTO Rooms
+VALUES(
+                16, "endoscopy suite", 1, "40,31,57,41"
+);
+
+INSERT INTO Rooms
+VALUES(
+                17, "estates department", 1, "27,11,19"
+);
+
+INSERT INTO Rooms
+VALUES(
+                18, "fracture clinic", 1, "54,24,44,47,21"
+);
+
+INSERT INTO Rooms
+VALUES(
+                19, "communicaitons and marketing", 1, "3,13,6,15,30"
+);
+
+INSERT INTO Rooms
+VALUES(
+                20, "GU Medicine", 1, "24,44"
+);
+
+INSERT INTO Rooms
+VALUES(
+                21, "pals", 1, "45,18,47,44,54"
+);
+
+INSERT INTO Rooms
+VALUES(
+                22, "hospital sterile disinfection unit", 1, "56,15,3,43,53"
+);
+
+INSERT INTO Rooms
+VALUES(
+                23, "hydrotherapy pool", 1, "42,13"
+);
+
+INSERT INTO Rooms
+VALUES(
+                24, "n/a", 1, "20,18,44"
+);
+
+INSERT INTO Rooms
+VALUES(
+                25, "jasmine centre", 1, "37"
+);
+
+INSERT INTO Rooms
+VALUES(
+                26, "laundry", 1, "55"
+);
+
+INSERT INTO Rooms
+VALUES(
+                27, "lister court", 1, "17"
+);
+
+INSERT INTO Rooms
+VALUES(
+                28, "management offices", 1, "11"
+);
+
+INSERT INTO Rooms
+VALUES(
+                29, "medical engineering department", 1, "30,53,43,15"
+);
+
+INSERT INTO Rooms
+VALUES(
+                30, "medical patients library", 1, "29,30,3"
+);
+
+INSERT INTO Rooms
+VALUES(
+                31, "medical photography", 1, "16,29,42"
+);
+
+INSERT INTO Rooms
+VALUES(
+                32, "medical photography", 1, "34,39,54"
+);
+
+INSERT INTO Rooms
+VALUES(
+                33, "aurora centre", 1, "56"
+);
+
+INSERT INTO Rooms
+VALUES(
+                34, "MRI", 1, "32"
+);
+
+INSERT INTO Rooms
+VALUES(
+                35, "chatsfield suite", 1, "39"
+);
+
+INSERT INTO Rooms
+VALUES(
+                36, "general office", 1, "3,56"
+);
+
+INSERT INTO Rooms
+VALUES(
+                37, "outpatients department", 1, "10"
+);
+
+INSERT INTO Rooms
+VALUES(
+                38, "parkhill hospital", 1, "41,57,2"
+);
+
+INSERT INTO Rooms
+VALUES(
+                39, "pharmacy", 1, "35,32,54"
+);
+
+INSERT INTO Rooms
+VALUES(
+                40, "education centre", 1, "16,57"
+);
+
+INSERT INTO Rooms
+VALUES(
+                41, "south block", 1, "16,31,29"
+);
+
+INSERT INTO Rooms
+VALUES(
+                42, "social services and discharge lounge", 1, "31,23"
+);
+
+INSERT INTO Rooms
+VALUES(
+                43, "dispensary pharmacy", 1, "53,4,15,38,22"
+);
+
+INSERT INTO Rooms
+VALUES(
+                44, "diagnostic day unit", 1, "24,20"
+);
+
+INSERT INTO Rooms
+VALUES(
+                45, "social work department", 1, "21,27"
+);
+
+INSERT INTO Rooms
+VALUES(
+                46, "speech therapy", 1, "38,4"
+);
+
+INSERT INTO Rooms
+VALUES(
+                47, "directoroate of people and organisational development", 1, "44,2,54"
+);
+
+INSERT INTO Rooms
+VALUES(
+                48, "squash court", 1, "10"
+);
+
+INSERT INTO Rooms
+VALUES(
+                49, "stores", 1, "52,10"
+);
+
+INSERT INTO Rooms
+VALUES(
+                50, "procurement department", 1, "6,7,51"
+);
+
+INSERT INTO Rooms
+VALUES(
+                51, "training unit", 1, "50"
+);
+
+INSERT INTO Rooms
+VALUES(
+                52, "training unit", 1, "1,49"
+);
+
+INSERT INTO Rooms
+VALUES(
+                53, "urology", 1, "29,43,15"
+);
+
+INSERT INTO Rooms
+VALUES(
+                54, "west ward block", 1, "32,56,18,21,47"
+);
+
+INSERT INTO Rooms
+VALUES(
+                55, "maternity ward", 1, "56,26"
+);
+
+INSERT INTO Rooms
+VALUES(
+                56, "WRVS shop", 1, "55,33,54"
+);
+
+INSERT INTO Rooms
+VALUES(
+                57, "x-ray", 1, "4540,16,38,2"
+);
